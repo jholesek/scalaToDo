5bc3f5dda59c6c0989b4fffc2426fc186df14da6
Jakob Holesek <jholesek@gmail.com>
2026-03-22 17:30:14 +0000

    added InputProcessor

---
+++ InputProcessor.scala
@@ -0,0 +1,41 @@
+import IOHelper._
+import Database._
+import scala.util.{Try, Success, Failure}
+
+class InputProcessor(db: Database) {
+
+        def handleView(): Try[Unit] = Try {
+                val res=db.selectAll()
+                res match {
+                        case Success(tasks) =>
+                                for((task, count) <- tasks.zip(Stream from 1))
+                                        println(s"${count}. $task")
+                        case Failure(e) =>
+                                System.err.println(e)
+                }
+
+        }
+
+        def handleAdd(task: String): Try[Unit] = {
+                db.insert(task)
+        }
+
+
+        def handleInput(input: String): Try[Unit] = input match {
+                case "q" =>
+                        Try{System.exit(0)}
+                case "h" =>
+                        IOHelper.showHelp()
+                case "v" =>
+                        handleView()
+                case add if add.startsWith("a ") =>
+                        handleAdd(add.drop(2))
+                        handleView()
+                case del if del.startsWith("d ") =>
+                        ???
+                case _ => ???
+
+                // println("handle: $s",input)
+        }
+}
+
